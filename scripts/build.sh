8e3e0618b91461781a9e2ab588db3963764b9ab2
satyrnwastaken <satyrn@disroot.org>
2022-04-24 17:17:15 +0000

    fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 commit=$(git log -1 | head -n 1 | sed -e "s/commit//")
 
 echo -e "building...\n"
-go build -ldflags "-X 'utils.Commit=$commit'" -v
+go build -ldflags "-X 'utils.Commit=$commit'" -v -o ./kosmos
